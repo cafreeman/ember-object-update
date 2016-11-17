e7381f61b66ac6ffe0a3442dbbb9352ccb6fcb15
Chris Freeman <cfreeman.code@gmail.com>
2016-11-17 13:34:45 -0600

    Add default export and update test imports

--- tests/dummy/app/components/simple-counter.js
+++ tests/dummy/app/components/simple-counter.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import layout from '../templates/components/simple-counter';
-import update from 'ember-object-update/utils/update';
+import update from 'ember-object-update';
 
 export default Ember.Component.extend({
   layout,
