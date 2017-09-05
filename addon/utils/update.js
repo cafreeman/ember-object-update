8f3e19f0efa078882ed0db9396fd08c390fbd178
Kelly Selden <kellyselden@gmail.com>
2017-09-05 13:47:51 -0700

    remove assign logic

--- addon/utils/update.js
+++ addon/utils/update.js
@@ -1,11 +1,10 @@
 import Ember from 'ember';
 
-const { get, setProperties, assign } = Ember;
+const { get, set, assign } = Ember;
 
 // Usage: update(person, 'name', (name) => name.toUpperCase())
 export default function update(obj, key, updateFn) {
   let property = get(obj, key);
   let newValue = updateFn(property);
-  let newObject = assign({}, obj, { [key]: newValue });
-  setProperties(obj, newObject);
+  set(obj, key, newValue);
 }
