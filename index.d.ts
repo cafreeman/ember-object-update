895db0a9219792477d94289ac3952323d6643225
Chris Krycho <chris@chriskrycho.com>
2017-12-13 16:13:45 -0700

    Add TypeScript type definition file.

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+// Type definitions for ember-object-update 0.5
+// Project: https://github.com/cafreeman/ember-object-update
+// Definitions by: Chris Krycho <https://github.com/chriskrycho>
+// Definitions: https://github.com/cafreeman/ember-object-update
+// TypeScript Version: 2.4
+
+import ComputedProperty from '@ember/object/computed';
+
+type ComputedProperties<T> = { [K in keyof T]: ComputedProperty<T[K]> | T[K] };
+
+// function get<T, K extends keyof T>(obj: ComputedProperties<T>, key: K): T[K];
+
+export default function update<T, K extends keyof T>(
+  obj: ComputedProperties<T>,
+  key: K,
+  updateFn: (property: T[K]) => T[K]
+): T;
